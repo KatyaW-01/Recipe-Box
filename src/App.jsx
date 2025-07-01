7eae3ca5a0bd46e8748b04cb894ed51caa58d053
Katya Weicht <katya.weicht@gmail.com>
2025-07-01 16:32:21 -0600

    Create routes for main pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from "react"
 import './App.css'
+import {Routes,Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Recipes from "./pages/Recipes"
+import ManageRecipes from "./pages/ManageRecipes"
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/recipes" element={<Recipes />} />
+      <Route path="/manageRecipes" element={<ManageRecipes />} />
+
+    </Routes> 
   )
 }
 
